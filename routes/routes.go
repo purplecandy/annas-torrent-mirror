a7c95e08bb84c0e5027170d666b602f3d3f20ebb
Nadeem <nadeem@kibibyte.in>
2024-08-29 01:35:40 +0530

    Added anna's archive mirror template

--- routes/routes.go
+++ routes/routes.go
@@ -10,13 +10,15 @@ import (
 type AllTorrentsGroupResult struct {
 	GroupName string
 	Count     uint
+	Bytes     string
+	Files     string
 }
 
 func AllTorrentGroups(c *fiber.Ctx) error {
 
 	var results []AllTorrentsGroupResult
 
-	err := database.DB.Model(&models.Torrent{}).Select("group_name, COUNT(*) as count").Group("group_name").Scan(&results).Error
+	err := database.DB.Model(&models.Torrent{}).Select("group_name, COUNT(*) as count, sum(data_size)::TEXT AS bytes, sum(num_files)::TEXT AS files").Group("group_name").Order("count asc").Scan(&results).Error
 
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{"message": err})
